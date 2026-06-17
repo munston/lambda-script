58170bd4892c9cbc19c5953467c851b735591cff
r-sta <151573323+munston@users.noreply.github.com>
2026-06-17 10:21:00 +0100

    Refresh generated hat installation backend

--- hat/installation_script.hs
+++ hat/installation_script.hs
@@ -2,26 +2,23 @@
 {- cabal:
 build-depends: base, directory, filepath, process
 -}
-
+-- generated-from: installation_script.hat
+-- hat-source-hash: c48cd949d3b5dbd3
 module Main where
-
-import System.Directory (createDirectoryIfMissing, getCurrentDirectory)
+import System.Directory (createDirectoryIfMissing, getCurrentDirectory, setCurrentDirectory)
 import System.Exit (ExitCode(..), exitWith)
 import System.FilePath ((</>))
 import System.Process (rawSystem)
-
 main :: IO ()
 main = do
   root <- getCurrentDirectory
-  let binDir = root </> "bin"
-  createDirectoryIfMissing True binDir
-  code <- rawSystem "cabal"
-    [ "install"
-    , "exe:hat"
-    , "--install-method=copy"
-    , "--overwrite-policy=always"
-    , "--installdir=" ++ binDir
-    ]
+  let installDir = root </> "bin"
+  createDirectoryIfMissing True installDir
+  runStep "cabal" ["install", "exe:hat", "--install-method=copy", "--overwrite-policy=always", "--installdir=" ++ installDir]
+  pure ()
+runStep :: String -> [String] -> IO ()
+runStep prog args = do
+  code <- rawSystem prog args
   case code of
-    ExitSuccess -> putStrLn ("hat installed to " ++ binDir) >> pure ()
+    ExitSuccess -> pure ()
     ExitFailure n -> exitWith (ExitFailure n)
