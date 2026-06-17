b3a8a4746aff1adac8ae33eac5ee016ab4923504
r-sta <151573323+munston@users.noreply.github.com>
2026-06-17 11:27:58 +0100

    Route sandbox config toward Hat scripts

--- projects/lambda-script/src/lambda-script/lib/LambdaScript/Sandbox/Config.hs
+++ projects/lambda-script/src/lambda-script/lib/LambdaScript/Sandbox/Config.hs
@@ -5,17 +5,20 @@ module LambdaScript.Sandbox.Config
   , TargetButton(..)
   , cfgToolRoot
   , cfgButton
+  , cfgHatRoot
+  , cfgOnepushHat
+  , cfgLandHat
   , loadConfig
   ) where
 
-import Data.Aeson ((.:), withObject, eitherDecodeFileStrict')
+import Data.Aeson ((.:), (.:?), withObject, eitherDecodeFileStrict')
 import Data.Aeson.Types (FromJSON(..), Parser)
 import qualified Data.Text as T
 import System.Directory (doesFileExist)
 import System.FilePath ((</>))
 
 data SandboxConfig =
-  SandboxConfig FilePath TargetButton
+  SandboxConfig FilePath TargetButton (Maybe FilePath) (Maybe FilePath) (Maybe FilePath)
   deriving (Eq, Show)
 
 newtype TargetButton =
@@ -23,16 +26,37 @@ newtype TargetButton =
   deriving (Eq, Show)
 
 cfgToolRoot :: SandboxConfig -> FilePath
-cfgToolRoot (SandboxConfig root _) = root
+cfgToolRoot (SandboxConfig root _ _ _ _) = root
 
 cfgButton :: SandboxConfig -> String
-cfgButton (SandboxConfig _ (TargetButton button)) = button
+cfgButton (SandboxConfig _ (TargetButton button) _ _ _) = button
+
+cfgHatRoot :: SandboxConfig -> FilePath
+cfgHatRoot cfg@(SandboxConfig _ _ hatRoot _ _) =
+  case hatRoot of
+    Just root -> root
+    Nothing -> cfgToolRoot cfg </> "hat"
+
+cfgOnepushHat :: SandboxConfig -> FilePath
+cfgOnepushHat cfg@(SandboxConfig _ _ _ onepushHat _) =
+  case onepushHat of
+    Just path -> path
+    Nothing -> cfgToolRoot cfg </> ("onepush-" ++ cfgButton cfg ++ ".hat")
+
+cfgLandHat :: SandboxConfig -> FilePath
+cfgLandHat cfg@(SandboxConfig _ _ _ _ landHat) =
+  case landHat of
+    Just path -> path
+    Nothing -> cfgToolRoot cfg </> "land-anything.hat"
 
 instance FromJSON SandboxConfig where
   parseJSON = withObject "SandboxConfig" $ \o -> do
     root <- requireText "tool_root" =<< o .: "tool_root"
     button <- requireText "button" =<< o .: "button"
-    pure (SandboxConfig root (TargetButton button))
+    hatRoot <- optionalText =<< o .:? "hat_root"
+    onepushHat <- optionalText =<< o .:? "onepush_hat"
+    landHat <- optionalText =<< o .:? "land_hat"
+    pure (SandboxConfig root (TargetButton button) hatRoot onepushHat landHat)
 
 requireText :: String -> T.Text -> Parser String
 requireText name raw =
@@ -40,6 +64,12 @@ requireText name raw =
   in if null value
        then fail (name ++ " must not be empty")
        else pure value
+
+optionalText :: Maybe T.Text -> Parser (Maybe String)
+optionalText Nothing = pure Nothing
+optionalText (Just raw) = do
+  let value = T.unpack raw
+  if null value then pure Nothing else pure (Just value)
 
 loadConfig :: FilePath -> IO SandboxConfig
 loadConfig cwd = do
